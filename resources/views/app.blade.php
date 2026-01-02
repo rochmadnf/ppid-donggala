0c4ff1592660e656291afa638fbb3ddd1883a8bf
rochmadnf <rochmadnf@gmail.com>
2026-01-02 17:27:23 +0800

    remove tittle tag on rootView

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,8 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title inertia>{{ config('app.name', 'My App') }}</title>
-
     {{-- Favicon --}}
     <link rel="apple-touch-icon-precomposed" sizes="57x57"
         href="{{ asset('assets/favicon/apple-touch-icon-57x57.png') }}" />
