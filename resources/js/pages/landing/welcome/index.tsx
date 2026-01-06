1249901584b07ef957457c3f64cb05274921a3e8
rochmadnf <rochmadnf@gmail.com>
2026-01-07 00:03:04 +0800

    eform section
    mobile view (393px)

--- resources/js/pages/landing/welcome/index.tsx
+++ resources/js/pages/landing/welcome/index.tsx
@@ -36,7 +36,7 @@ export default function WelcomePage() {
             </div>
 
             {/* Information Categories */}
-            <div className="relative z-1 mt-12 flex min-h-105 w-full flex-col items-center justify-start gap-x-0 gap-y-6 overflow-x-auto overflow-y-hidden rounded-b-[2.5rem] bg-white px-4 py-6 pt-24 shadow-bottom-md shadow-slate-950/15 md:mt-28 md:flex-row md:justify-center md:gap-x-8 md:gap-y-0 md:px-6">
+            <div className="relative z-1 mt-12 flex min-h-105 w-full flex-col items-center justify-start gap-x-0 gap-y-6 overflow-x-auto overflow-y-hidden rounded-b-4xl bg-white px-4 py-6 pt-24 shadow-bottom-md shadow-slate-950/15 md:mt-28 md:flex-row md:justify-center md:gap-x-8 md:gap-y-0 md:rounded-b-[2.5rem] md:px-6">
                 <TitleDivider title="Informasi Publik" className="absolute top-4 left-4 md:left-6" />
                 {menus.map((menu) => (
                     <PublicInfoCard key={menu.id} menu={menu} />
@@ -45,40 +45,42 @@ export default function WelcomePage() {
             </div>
 
             {/* E-Form Hero */}
-            <div className="-mt-20 grid min-h-50 grid-cols-12 bg-linear-to-r from-blue-700 to-blue-400 px-6 pt-38 pb-28 text-white">
+            <div className="-mt-20 grid min-h-50 grid-cols-12 bg-linear-to-tr from-blue-700 to-blue-400 px-6 pt-32 pb-32 text-white md:bg-linear-to-r md:pt-38 md:pb-28">
                 <div className="col-span-full md:col-span-7">
                     <h6 className="flex w-fit items-center rounded-full border border-white px-4 py-1.5 text-xs tracking-wide">
                         <span className="mr-2 size-1.5 animate-pulse rounded-full bg-yellow-400"></span>E - FORM
                     </h6>
 
-                    <h1 className="mt-8 text-5xl leading-16 font-bold">Pengajuan Informasi Publik Kini Lebih Mudah</h1>
+                    <h1 className="mt-6 text-4xl leading-12 font-bold md:mt-8 md:text-5xl md:leading-16">
+                        Pengajuan Informasi Publik Kini Lebih Mudah
+                    </h1>
 
                     <p className="mt-2 text-lg text-white/95">
                         Ajukan permohonan Informasi Publik, atau Keberatan Informasi Publik dengan mengisi formulir elektronik.
                     </p>
 
-                    <div className="mt-8 flex items-center gap-x-4">
+                    <div className="mt-8 flex flex-col gap-x-0 gap-y-6 md:flex-row md:items-center md:gap-x-4 md:gap-y-0">
                         <ButtonFlatIcon asChild>
                             <Link
                                 href={route('welcome')}
-                                className="group flex-col bg-white text-slate-950 transition-colors duration-200 ease-out hover:bg-white/90"
+                                className="group flex-col items-start bg-white text-slate-950 transition-colors duration-200 ease-out hover:bg-white/90 md:items-center"
                             >
                                 <small className="self-start text-[13.5px]/[1.43] text-slate-700">Ajukan</small>
                                 <span className="inline-flex items-center gap-x-1 font-bold tracking-wide whitespace-nowrap">
                                     Permohonan Informasi{' '}
-                                    <MoveRightIcon className="pointer-events-none size-4 transition-transform duration-200 ease-out group-hover:translate-x-0.5" />
+                                    <MoveRightIcon className="pointer-events-none hidden size-4 transition-transform duration-200 ease-out group-hover:translate-x-0.5 md:inline" />
                                 </span>
                             </Link>
                         </ButtonFlatIcon>
                         <ButtonFlatIcon asChild>
                             <Link
                                 href={route('welcome')}
-                                className="group flex-col border border-white text-white transition-colors duration-200 ease-out hover:bg-white hover:text-slate-950"
+                                className="group flex-col items-start border border-white text-white transition-colors duration-200 ease-out hover:bg-white hover:text-slate-950 md:items-center"
                             >
                                 <small className="self-start text-[13.5px]/[1.43] text-white/90 group-hover:text-slate-700">Ajukan</small>
                                 <span className="inline-flex items-center gap-x-1 font-bold tracking-wide whitespace-nowrap">
                                     Keberatan Informasi{' '}
-                                    <MoveRightIcon className="pointer-events-none size-4 transition-transform duration-200 ease-out group-hover:translate-x-0.5" />
+                                    <MoveRightIcon className="pointer-events-none hidden size-4 transition-transform duration-200 ease-out group-hover:translate-x-0.5 md:inline" />
                                 </span>
                             </Link>
                         </ButtonFlatIcon>
@@ -91,7 +93,7 @@ export default function WelcomePage() {
             </div>
 
             {/* News Section */}
-            <div className="z-2 -mt-20 min-h-30 rounded-t-[2.5rem] bg-white px-6"></div>
+            <div className="z-2 -mt-20 min-h-30 rounded-t-4xl bg-white px-6 md:rounded-t-[2.5rem]"></div>
         </>
     );
 }
