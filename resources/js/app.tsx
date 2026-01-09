acd938465aa5627b6dd3c12e6fa81c134b571174
rochmadnf <rochmadnf@gmail.com>
2026-01-09 21:34:46 +0800

    hydrate client for support ssr

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -2,7 +2,7 @@ import '../css/app.css';
 
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createRoot } from 'react-dom/client';
+import { hydrateRoot } from 'react-dom/client';
 
 const appName = import.meta.env.VITE_APP_NAME || 'My App';
 
@@ -10,9 +10,7 @@ createInertiaApp({
     title: (title) => (title ? title : appName),
     resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
     setup({ el, App, props }) {
-        const root = createRoot(el);
-
-        root.render(<App {...props} />);
+        hydrateRoot(el, <App {...props} />);
     },
     progress: {
         color: '#EF4444',
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="id">
 
 <head>
+    {{-- Meta Default --}}
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
