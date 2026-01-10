d0139cfbd066f7ae610d509096a1c07cfd23a47b
rochmadnf <rochmadnf@gmail.com>
2026-01-10 11:02:46 +0800

    conditional to hydrate when env production

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -2,15 +2,20 @@ import '../css/app.css';
 
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { hydrateRoot } from 'react-dom/client';
+import { createRoot, hydrateRoot } from 'react-dom/client';
 
 const appName = import.meta.env.VITE_APP_NAME || 'My App';
+const appEnv = import.meta.env.VITE_APP_ENV || 'production';
 
 createInertiaApp({
     title: (title) => (title ? title : appName),
     resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
     setup({ el, App, props }) {
-        hydrateRoot(el, <App {...props} />);
+        if (appEnv === 'production') {
+            hydrateRoot(el, <App {...props} />);
+        } else {
+            createRoot(el).render(<App {...props} />);
+        }
     },
     progress: {
         color: '#EF4444',
