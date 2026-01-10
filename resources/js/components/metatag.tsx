7985c8d6b4a84f0a85294d2c2f6db434c3ba2c4b
rochmadnf <rochmadnf@gmail.com>
2026-01-10 22:16:01 +0800

    add default image for metatag

--- resources/js/components/metatag.tsx
+++ resources/js/components/metatag.tsx
@@ -1,3 +1,4 @@
+import { appAsset } from '@/lib/utils';
 import { Head } from '@inertiajs/react';
 import type { PropsWithChildren } from 'react';
 
@@ -5,21 +6,31 @@ export interface MetaTagProps {
     title: string;
     description: string;
     type?: string;
+    image?: string;
 }
 
-export function MetaTag({ title, description, type = 'website', children }: PropsWithChildren<MetaTagProps>) {
+export function MetaTag({
+    title,
+    description,
+    type = 'website',
+    image = '/assets/img/welcome-card.webp',
+    children,
+}: PropsWithChildren<MetaTagProps>) {
     return (
         <Head title={title}>
-            <meta name="description" content={description} />
+            <meta name="description" content={description} itemProp="description" />
+
             {/* Open Graph */}
             <meta property="og:title" content={title} />
             <meta property="og:description" content={description} />
             <meta property="og:type" content={type} />
+            <meta property="og:image" content={appAsset(image)} />
 
             {/* Twitter */}
             <meta name="twitter:card" content="summary_large_image" />
             <meta name="twitter:title" content={title} />
             <meta name="twitter:description" content={description} />
+            <meta name="twitter:image" content={appAsset(image)} />
 
             {children}
         </Head>
