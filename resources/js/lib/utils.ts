1da10ea6d19a26f2516624f474e1bd50cb0f101f
rochmadnf <rochmadnf@gmail.com>
2026-01-10 22:15:39 +0800

    new lib function appAsset to get asset url

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export function appAsset(path: string) {
+    const appUrl = import.meta.env.VITE_APP_URL || 'http://localhost';
+    return `${appUrl}/${path.replace(/^\/+/, '')}`;
+}
