126ca054fef635a4b030276bd83807d6e7b43756
rochmadnf <rochmadnf@gmail.com>
2026-01-15 21:44:51 +0800

    refactor login page

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -1,9 +1,8 @@
-import { InputErrorMessage } from '@/components/input-error-message';
+import { FormInput } from '@/components/form/input';
+import { MetaTag } from '@/components/metatag';
 import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import AuthLayout from '@/layouts/auth-layout';
-import { Head, useForm } from '@inertiajs/react';
+import { useForm } from '@inertiajs/react';
 import { type FormEventHandler } from 'react';
 
 export default function Login() {
@@ -25,44 +24,36 @@ export default function Login() {
 
     return (
         <AuthLayout title="Masuk" description="Silakan masukkan kredensial untuk melanjutkan.">
-            <Head title="Masuk">
-                <meta name="description" content="Halaman untuk masuk ke aplikasi PPID Kabupaten Donggala." />
-            </Head>
+            <MetaTag withAppName title="Masuk" description="Halaman untuk masuk ke aplikasi PPID Kabupaten Donggala.">
+                <meta name="robots" content="noindex, nofollow" />
+
+                <meta property="og:url" content={route('login')} />
+                <link rel="canonical" href={route('login')} />
+            </MetaTag>
 
             <form onSubmit={hitLoginButton} className="space-y-6">
-                <div>
-                    <Label htmlFor="username">Nama Pengguna</Label>
-                    <Input
-                        id="username"
-                        type="text"
-                        name="username"
-                        className="mt-2.5"
-                        tabIndex={1}
-                        value={form.data.username}
-                        autoComplete="username"
-                        autoFocus
-                        onChange={(e) => form.setData('username', e.target.value)}
-                    />
-                    <InputErrorMessage message={form.errors.username} className="mt-2" />
-                </div>
+                <FormInput
+                    label="Nama Pengguna"
+                    name="username"
+                    autoFocus
+                    autoComplete="username"
+                    value={form.data.username}
+                    onChange={(e) => form.setData('username', e.target.value)}
+                    error={form.errors.username}
+                />
 
-                <div>
-                    <Label htmlFor="password">Katasandi</Label>
-                    <Input
-                        id="password"
-                        name="password"
-                        type="password"
-                        className="mt-2.5"
-                        tabIndex={2}
-                        value={form.data.password}
-                        autoComplete="current-password"
-                        onChange={(e) => form.setData('password', e.target.value)}
-                    />
-                    <InputErrorMessage message={form.errors.password} className={'mt-2'} />
-                </div>
+                <FormInput
+                    label="Katasandi"
+                    type="password"
+                    name="password"
+                    autoComplete="current-password"
+                    value={form.data.password}
+                    onChange={(e) => form.setData('password', e.target.value)}
+                    error={form.errors.password}
+                />
 
                 <div className="flex items-center justify-between">
-                    <Button variant="brand" disabled={form.processing}>
+                    <Button variant="brand" className="h-12 w-full cursor-pointer text-lg" disabled={form.processing}>
                         Masuk
                     </Button>
                 </div>
