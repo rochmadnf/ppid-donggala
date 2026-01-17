06d92a686f73b59be12a70b24679449ddce836c1
rochmadnf <rochmadnf@gmail.com>
2026-01-17 21:07:48 +0800

    refactor users table and make user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $s = \App\Models\User::create([
+            'username' => 'superadmin',
+            'email' => 'rochmadnf@donggala.go.id',
+            'password' => 'P4$$w0rd',
+        ]);
+    }
+}
