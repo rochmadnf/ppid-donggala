68d7590374f76b22731ddbd120f566e62be6f352
rochmadnf <rochmadnf@gmail.com>
2026-04-26 15:27:40 +0800

    shorthand condition

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') === 'production') {
+        if (app()->isProduction()) {
             \Illuminate\Support\Facades\URL::forceScheme('https');
         }
 
