0c92c9590f3e8c22d194fe646ce3d0b7c8ca9f8b
rochmadnf <rochmadnf@gmail.com>
2026-05-11 01:29:21 +0800

    update searchByKeyword default column behavior

--- app/Models/Scopes/SearchableScope.php
+++ app/Models/Scopes/SearchableScope.php
@@ -12,18 +12,20 @@ trait SearchableScope
      * Scope to filter by keyword search.
      *
      * @param  Builder  $query
-     * @param  string  $defaultColumn  The default column to search if 'search_by' is not provided.
+     * @param  string|array  $defaultColumns  The default columns to search if 'search_by' is not provided.
      * @return void
      */
-    public function scopeSearchByKeyword(Builder $query, string $defaultColumn = 'name'): void
+    public function scopeSearchByKeyword(Builder $query, string|array $defaultColumns = 'name'): void
     {
         $query->when(
             request()->has('keyword') && strlen(request()->input('keyword')) > 0,
-            fn(Builder $q) => $q->where(
-                column: request()->input('search_by', $defaultColumn),
-                operator: 'LIKE',
-                value: '%' . request()->input('keyword') . '%'
-            )
+            fn(Builder $q)  => $q->when(is_array($defaultColumns), function (Builder $_q) use ($defaultColumns) {
+                $_q->where(function (Builder $q) use ($defaultColumns) {
+                    foreach ($defaultColumns as $column) {
+                        $q->orWhere($column, 'LIKE', '%' . request()->input('keyword') . '%');
+                    }
+                });
+            })
         );
     }
 }
