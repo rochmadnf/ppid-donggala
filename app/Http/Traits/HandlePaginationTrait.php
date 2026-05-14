a63203bb67a76c72b503ec7d88562e899b1bf176
rochmadnf <rochmadnf@gmail.com>
2026-05-14 21:36:18 +0800

    fix error when delete in first page without params

--- app/Http/Traits/HandlePaginationTrait.php
+++ app/Http/Traits/HandlePaginationTrait.php
@@ -16,25 +16,31 @@ trait HandlePaginationTrait
         $previousUrl = url()->previous();
 
         // Ambil query string dari URL sebelumnya
-        parse_str(parse_url($previousUrl, PHP_URL_QUERY), $query);
+        $parseUrl = parse_url($previousUrl, PHP_URL_QUERY);
 
-        $currentPage = (int) ($query['page'] ?? 1);
-        $perPage     = (int) ($query['per_page'] ?? $defaultPerPage);
+        $newParams = [];
 
-        $lastPage   = max((int) ceil($remaining / $perPage), 1);
-        $targetPage = min($currentPage, $lastPage);
+        if (!is_null($parseUrl)) {
 
-        $newParams = array_merge($query, [
-            'page'     => $targetPage,
-            'per_page' => $perPage,
-        ], $extraParams);
+            parse_str($parseUrl, $query);
+
+            $currentPage = (int) ($query['page'] ?? 1);
+            $perPage     = (int) ($query['per_page'] ?? $defaultPerPage);
+
+            $lastPage   = max((int) ceil($remaining / $perPage), 1);
+            $targetPage = min($currentPage, $lastPage);
+
+            $newParams = array_merge($query, [
+                'page'     => $targetPage,
+                'per_page' => $perPage,
+            ], $extraParams);
+        }
 
         if (is_null($routeName)) {
             $routeName = app('router')->getRoutes()->match(
                 request()->create($previousUrl, 'GET')
             )->getName();
         }
-
 
         return redirect()->route($routeName, $newParams);
     }
