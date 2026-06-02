a4df00232fb332b66edde789109671d33bcfcbb7
rochmadnf <rochmadnf@gmail.com>
2026-06-03 03:34:11 +0800

    set sizing variant

--- resources/js/components/form/input.tsx
+++ resources/js/components/form/input.tsx
@@ -5,21 +5,28 @@ import { Input } from '../ui/input';
 import { Label } from '../ui/label';
 import type { FormInputProps } from './types';
 
-export function FormInput({ label, error, className, ...props }: Omit<ComponentProps<'input'>, 'form'> & FormInputProps) {
+export function FormInput({
+    label,
+    error,
+    className,
+    inputSize = 'default',
+    wrapperClassName,
+    ...props
+}: Omit<ComponentProps<'input'>, 'form'> & FormInputProps) {
     return (
-        <div>
-            <Label htmlFor={props.name} className="text-sm leading-4 md:text-[15px] lg:text-[17px]">
-                {label}
+        <div className={cn('space-y-2', wrapperClassName)}>
+            <Label htmlFor={props.name} className={cn(inputSize === 'lg' ? 'text-sm leading-4 md:text-[15px] lg:text-[17px]' : '', className)}>
+                {label} {props.required ? <span className="align-middle text-destructive">*</span> : null}
             </Label>
             <Input
                 id={props.name}
                 className={cn(
-                    `mt-2.5 h-10 border-line-brand/80 md:h-12 md:text-lg lg:mt-3`,
-                    'focus-visible:ring-[2.5px] focus-visible:ring-offset-2',
-                    'selection:bg-blue-600 focus-visible:border-blue-500/45 focus-visible:ring-blue-500/75',
+                    inputSize === 'lg'
+                        ? 'mt-2.5 h-10 border-line-brand/80 selection:bg-blue-600 focus-visible:border-blue-500/45 focus-visible:ring-[2.5px] focus-visible:ring-blue-500/75 focus-visible:ring-offset-2 md:h-12 md:text-lg lg:mt-3'
+                        : 'h-9.5',
                     className,
                 )}
-                tabIndex={2}
+                aria-invalid={!!error}
                 {...props}
             />
             <InputErrorMessage message={error} className={'mt-2'} />
