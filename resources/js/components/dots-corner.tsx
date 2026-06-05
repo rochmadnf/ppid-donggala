3d4394a7df10b63a7aacae59e26030bb806c5084
rochmadnf <rochmadnf@gmail.com>
2026-06-06 03:52:09 +0800

    component ui: dots on top corner

---
+++ resources/js/components/dots-corner.tsx
@@ -0,0 +1,8 @@
+export function DotsCorner() {
+    return (
+        <>
+            <div className="absolute -top-[4px] -left-[4px] size-[7px] bg-ppid-primary" aria-hidden="true"></div>
+            <div className="absolute -top-[4px] -right-[4px] size-[7px] bg-ppid-primary" aria-hidden="true"></div>
+        </>
+    );
+}
