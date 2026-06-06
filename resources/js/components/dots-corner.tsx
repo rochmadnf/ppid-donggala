ad3ee8f78106425c17046a3d785396b1ddabd5e2
rochmadnf <rochmadnf@gmail.com>
2026-06-06 09:50:01 +0800

    set side to dotscorner

--- resources/js/components/dots-corner.tsx
+++ resources/js/components/dots-corner.tsx
@@ -1,8 +1,17 @@
-export function DotsCorner() {
+import { cn } from '@/lib/utils';
+
+const sides = {
+    top: '-top-[4px]',
+    bottom: '-bottom-[4px]',
+} as const;
+
+export function DotsCorner({ side = 'top' }: { side?: keyof typeof sides }) {
+    const positionClass = sides[side];
+
     return (
         <>
-            <div className="absolute -top-[4px] -left-[4px] size-[7px] bg-ppid-primary" aria-hidden="true"></div>
-            <div className="absolute -top-[4px] -right-[4px] size-[7px] bg-ppid-primary" aria-hidden="true"></div>
+            <div className={cn('absolute -left-[4px] z-99 size-[7px] bg-ppid-primary', positionClass)} aria-hidden="true" />
+            <div className={cn('absolute -right-[4px] z-99 size-[7px] bg-ppid-primary', positionClass)} aria-hidden="true" />
         </>
     );
 }
