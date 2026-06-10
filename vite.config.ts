49512080fcb74556b558dd1bbcc538af938790f6
rochmadnf <rochmadnf@gmail.com>
2026-06-10 09:31:32 +0800

    remove manual chunks

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,12 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { bunny } from 'laravel-vite-plugin/fonts';
-import { execSync } from 'node:child_process';
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
     define: {
-        __ASSET_VERSION__: JSON.stringify(execSync('date +%s').toString().trim()),
+        __ASSET_VERSION__: JSON.stringify(Date.now().toString()),
     },
     plugins: [
         laravel({
@@ -55,33 +54,8 @@ export default defineConfig({
         sourcemap: process.env.VITE_SOURCE_MAP === 'true',
         chunkSizeWarningLimit: 2000,
         assetsInlineLimit: 0,
-        rollupOptions: {
-            output: {
-                manualChunks: (id): string | void => {
-                    if (id.includes('@embedpdf/react-pdf-viewer')) return 'pdf-engine';
-
-                    if (
-                        id.includes('@radix-ui/react-dialog') ||
-                        id.includes('@radix-ui/react-dropdown-menu') ||
-                        id.includes('@radix-ui/react-slot') ||
-                        id.includes('@radix-ui/react-label')
-                    )
-                        return 'radix-ui';
-
-                    if (id.includes('@tiptap/core') || id.includes('@tiptap/react')) return 'tiptap-core';
-                    if (id.includes('@tiptap/')) return 'tiptap-ext';
-
-                    if (id.includes('@embedpdf/plugin-')) return 'embedpdf-plugins';
-
-                    if (id.includes('lucide-react')) return 'icons';
-                },
-            },
-        },
     },
     optimizeDeps: {
         include: ['react', 'react-dom', '@inertiajs/react', 'lucide-react', '@embedpdf/react-pdf-viewer'],
-    },
-    worker: {
-        format: 'es',
     },
 });
